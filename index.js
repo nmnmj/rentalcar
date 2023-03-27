4556deb00fc48ea31fbaa6097bf3ca6d8584dcb2
nmnmj <nmnjay@gmail.com>
2023-03-27 11:21:00 +0530

    cc

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+const app = express()
+app.use(cors())
 import dotenv  from 'dotenv'
 import cookieParser from 'cookie-parser'
 import web from './routes/web.js'
 dotenv.config()
 import connectDB from './db/connectDB.js'
 const port = process.env.PORT
-const app = express()
-app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
