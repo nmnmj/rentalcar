124efb1b2c175792a7e79e7f9d35c96a052dbbc2
nmnmj <nmnjay@gmail.com>
2023-03-27 13:08:02 +0530

    f

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ dotenv.config()
 import connectDB from './db/connectDB.js'
 const app = express()
 app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 app.use(express.urlencoded({extended:false}))
 const port = process.env.PORT
 app.use(express.json())
