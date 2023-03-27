cfc42d9997445fa69b48d5ba5a00815e989653a2
nmnmj <nmnjay@gmail.com>
2023-03-27 12:49:03 +0530

    sf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 const port = process.env.PORT
 app.use(express.json())
-// app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:false}))
 connectDB(process.env.DATABASE_URL)
 app.use("/", web)
 
